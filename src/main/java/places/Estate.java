6758f50dd4e48068dec1234e22f85d3c69a0bec4
tronerta <tronerta.web@gmail.com>
2020-03-28 20:06:59 +0100

    Something

--- src/main/java/places/Estate.java
+++ src/main/java/places/Estate.java
@@ -1,9 +1,14 @@
 package places;
 
+import helpers.DateHelper;
 import objects.Item;
 import person.Person;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Estate {
@@ -19,13 +24,13 @@ public class Estate {
         this.people = people;
     }
 
-    public List<Place> getFreePlaces(){
+    public List<Place> getFreePlaces() {
         List<Place> freePlaces = new ArrayList<>(getFreeApartments());
         freePlaces.addAll(getFreeParkingSpots());
         return freePlaces;
     }
 
-    public List<Apartment> getFreeApartments(){
+    public List<Apartment> getFreeApartments() {
         List<Apartment> freeApartments = new ArrayList<>();
         for (Apartment a : apartments)
             if (a.isAvaliable())
@@ -33,7 +38,7 @@ public class Estate {
         return freeApartments;
     }
 
-    public List<ParkingSpot> getFreeParkingSpots(){
+    public List<ParkingSpot> getFreeParkingSpots() {
         List<ParkingSpot> freeParkingSpots = new ArrayList<>();
         for (ParkingSpot ps : parkings)
             if (ps.isAvaliable())
@@ -41,11 +46,28 @@ public class Estate {
         return freeParkingSpots;
     }
 
-    public List<Item> getFreeItems(){
+    public List<Item> getFreeItems() {
         List<Item> items = new ArrayList<>();
         for (Item i : this.items)
             if (!i.placed)
                 items.add(i);
         return items;
+    }
+
+    public void saveReport() {
+        try {
+            FileWriter file = new FileWriter("report_" + DateHelper.todayDate.toString() + ".txt");
+            file.write("ESTATE REPORT \n");
+            for (Person p : people) {
+                p.places.sort((lhs, rhs) -> Double.compare(rhs.volume, lhs.volume));
+
+            }
+            file.write("Files in Java might be tricky, but it is fun enough!");
+            file.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
